b20f53344902e38db801dd91767ddd4c1c2f630b
billh0420 <61120583+billh0420@users.noreply.github.com>
2022-02-04 15:24:41 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "WHCrossPlatformKit", path: "../WHCrossPlatformKit"),
-        .package(name: "WHPlayingCardKit", path: "../WHPlayingCardKit"),
-        .package(name: "WHPlayingCardImageKit", path: "../WHPlayingCardImageKit")
+        .package(url: "https://github.com/billh0420/WHCrossPlatformKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/billh0420/WHPlayingCardKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/billh0420/WHPlayingCardImageKit.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
